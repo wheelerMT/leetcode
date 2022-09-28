7cce19ce638cd35a69e2d4a079fe998064ff4936
Matt Wheeler <56673431+wheelerMT@users.noreply.github.com>
2022-09-28 19:51:33 +0100

    Time: 167 ms (28.28%), Space: 42 MB (54.38%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end());
-        int* start = &people[0];
-        int* end = &people[people.size() - 1];
+        int start = 0;
+        int end = people.size() - 1;
         int numOfBoats {};
         
         while (start <= end)
         {
-            if (*start + *end <= limit)
+            if (people[start] + people[end] <= limit)
             {
-                ++numOfBoats;
                 ++start;
                 --end;
             } else
             {
-                ++numOfBoats;
                 --end;
             }
+            ++numOfBoats;
         }
         
         return numOfBoats;
